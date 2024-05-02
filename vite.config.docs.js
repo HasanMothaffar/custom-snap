a35d91dc6f144a12d32211a9b146c4c64551e49d
HasanMothaffar <hasan.mozafar@gmail.com>
2024-05-02 09:07:19 +0300

    Update vite base URL

--- vite.config.docs.js
+++ vite.config.docs.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/custom-snap-SyrianOpenSource/",
-    build: {
-        outDir: "dist-docs"        
-    }
+	base: "/custom-snap/",
+	build: {
+		outDir: "dist-docs",
+	},
 });
